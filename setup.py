5391552fe40bb9881a00ae6607bf0b6737a5c2b1
itasli <ilyas.tasli@outlook.fr>
2021-04-13 15:15:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -13,5 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/itasli/fivempy",
     packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Indepented",
+    ],
     python_requires='>=3.6',
 )
